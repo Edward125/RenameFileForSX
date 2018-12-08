6de27823ed41ba7503be8a2bf76660238e494094
Edward <edward_song@yeah.net>
2018-12-08 17:59:04 +0800

    first commit

---
+++ RenameFileForSX/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace RenameFileForSX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lstInfo = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.txtFolder);
+            this.groupBox1.Location = new System.Drawing.Point(27, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(497, 52);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(78, 18);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(306, 21);
+            this.txtFolder.TabIndex = 0;
+            this.txtFolder.DoubleClick += new System.EventHandler(this.txtFolder_DoubleClick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(400, 14);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 30);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "文件夹地址";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lstInfo);
+            this.groupBox2.Location = new System.Drawing.Point(27, 82);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(497, 453);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // lstInfo
+            // 
+            this.lstInfo.FormattingEnabled = true;
+            this.lstInfo.ItemHeight = 12;
+            this.lstInfo.Location = new System.Drawing.Point(8, 20);
+            this.lstInfo.Name = "lstInfo";
+            this.lstInfo.Size = new System.Drawing.Size(483, 424);
+            this.lstInfo.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 547);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox lstInfo;
+    }
+}
+
---
+++ RenameFileForSX/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using Edward;
+
+namespace RenameFileForSX
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public class DirectoryAllFiles
+        {
+            static List<FileInformation> FileList = new List<FileInformation>();
+            public static List<FileInformation> GetAllFiles(DirectoryInfo dir)
+            {
+                FileInfo[] allFile = dir.GetFiles();
+                foreach (FileInfo fi in allFile)
+                {
+                    FileList.Add(new FileInformation { FileName = fi.Name, FilePath = fi.FullName, FileDirectory = fi.DirectoryName, FileExtension = fi.Extension });
+                }
+                DirectoryInfo[] allDir = dir.GetDirectories();
+                foreach (DirectoryInfo d in allDir)
+                {
+                    GetAllFiles(d);
+                }
+                return FileList;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public class FileInformation
+        {
+            public string FileName { get; set; }
+            public string FilePath { get; set; }
+            public string FileDirectory { get; set; }
+            public string FileExtension { get; set; }
+        }
+
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "批量修改文件名For山西改造,Ver:" + Application.ProductVersion;
+
+            txtFolder.SetWatermark("双击此处打开文件夹");
+        }
+
+        private void txtFolder_DoubleClick(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+                txtFolder.Text = fbd.SelectedPath;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtFolder.Text.Trim()))
+            {
+                updateMessage(lstInfo, "文件夹地址为空,请重新选择.");
+                txtFolder.Focus();
+                return;
+            }
+            if (!Directory.Exists(txtFolder.Text.Trim()))
+            {
+                updateMessage(lstInfo, "文件夹地址不存在,请重新输入.");
+                txtFolder.SelectAll();
+                txtFolder.Focus();
+                return;
+            }
+            lstInfo.Items.Clear();
+            long filecount = 0;
+            List<FileInformation> list = new List<FileInformation>();
+            list = DirectoryAllFiles.GetAllFiles(new System.IO.DirectoryInfo(@txtFolder.Text.Trim()));
+ 
+            foreach (var item in list)
+            {
+
+                //FIlE0001类
+                string newname = string.Empty;
+                if (item.FileName.ToLower().Trim().Contains("file"))
+                {
+                    string[] temp = item.FilePath.Split('\\');
+                    newname = temp[temp.Length - 3] + "@" + temp[temp.Length - 2] + "_" + item.FileName.ToUpper().Replace("FILE", "");
+                  //  File.Move(item.FilePath, item.FileDirectory + @"\" + newname);
+                }
+                //
+                if (item.FileName.Contains("_")   && !item.FileName.Contains ("@"))
+                {
+                    string[] tempfile = item.FileName.Replace(item.FileExtension, "").Split('_');
+                    if (tempfile.Length == 5)    //H_20170817013502_CT00000_123456_0001
+                    {
+                        string[] temp = item.FilePath.Split('\\');
+                        newname = temp[temp.Length - 3] + "@" + tempfile[1] + "_" + tempfile[tempfile.Length - 1] + item.FileExtension;
+                        // File.Move(item.FilePath, item.FileDirectory + @"\" + newname);
+
+                    }
+
+                    if (tempfile.Length == 3)
+                    {
+
+                        if (tempfile[tempfile.Length - 1].Length == 5) //8176162_20180513043852_00011
+                        {
+                            string[] temp = item.FilePath.Split('\\');
+                            newname = temp[temp.Length - 3] + "@" + tempfile[1] + "_" + tempfile[tempfile.Length - 1].Substring(1, 4) + item.FileExtension;
+
+                        }
+
+                        if (tempfile[tempfile.Length - 1].Length == 4)    //J800000_00000020170730100947_0016
+                        {
+                            string[] temp = item.FilePath.Split('\\');
+                            newname = temp[temp.Length - 3] + "@" + tempfile[1].Substring(6, 14) + "_" + tempfile[tempfile.Length - 1] + item.FileExtension;
+
+                        }
+
+                    }
+
+                }
+
+                if (!string.IsNullOrEmpty(newname) && !File.Exists(newname))
+                {
+                    try
+                    {
+                        filecount++;
+                        updateMessage(lstInfo, "正在处理:" + item.FileName);
+                        Application.DoEvents();
+                        File.Move(item.FilePath, item.FileDirectory + @"\" + newname);
+                    }
+                    catch (Exception ex)
+                    {
+
+                        updateMessage(lstInfo, ex.Message);
+                    }
+
+                }
+            }
+
+
+            MessageBox.Show("处理完毕,共计处理:" + filecount + "个文件");
+
+        }
+
+
+
+
+
+        #region 更新信息
+        /// <summary>
+        /// 更新信息到listbox中
+        /// </summary>
+        /// <param name="listbox">listbox name</param>
+        /// <param name="message">message</param>
+        public static void updateMessage(ListBox listbox, string message)
+        {
+            if (listbox.Items.Count > 1000)
+                listbox.Items.RemoveAt(0);
+
+            string item = string.Empty;
+            //listbox.Items.Add("");
+            item = DateTime.Now.ToString("HH:mm:ss") + " " + @message;
+            if (listbox.InvokeRequired)
+            {
+                listbox.BeginInvoke(new Action<string>((msg) =>
+                {
+                    listbox.Items.Add(msg);
+                }), item);
+
+            }
+            else
+            {
+                listbox.Items.Add(item);
+            }
+            if (listbox.Items.Count > 1)
+            {
+                listbox.TopIndex = listbox.Items.Count - 1;
+                listbox.SetSelected(listbox.Items.Count - 1, true);
+            }
+
+        }
+        #endregion
+
+
+
+
+
+
+    }
+}
